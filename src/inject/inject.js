90c79510dce6d862d6e5026c4baed78eda5544b2
Blake Campbell <blakecampbell.dev@gmail.com>
2018-09-27 14:21:29 -0700

    Updating to work with kanban board changes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,22 +1,18 @@
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
-      // chrome.alarms.create('UpdatedSelectedInfo', { delayInMinutes: 0.1 });
-      // chrome.alarms.onAlarm.addListener(function(alarm) {
-      //   alert("Beep");
-      // });
       clearInterval(readyStateCheckInterval);
-        // ----------------------------------------------------------
-        // This part of the script triggers when page is done loading
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
       shiftEventListener();
       checkIfKanBanBoard();
       var currentUrl = window.location.href
       setInterval(function(){
-        if(currentUrl != window.location.href){
+        if (currentUrl != window.location.href) {
           currentUrl = window.location.href;
           checkIfKanBanBoard();
         }
-      }, 3000)
+      }, 5000);
     }
   }, 10);
 });
@@ -24,30 +20,26 @@ chrome.extension.sendMessage({}, function(response) {
 function shiftEventListener () {
   window.addEventListener("click", function(e) {
     if (e.shiftKey) {
-      chrome.extension.sendMessage({type: 'table'}, function(response) {
-        calculateTotal();
-      });
+      chrome.extension.sendMessage({ type: 'table' }, function(response) { calculateTotal(); });
     };
   }, false);
+  document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    checkIfKanBanBoard();
+  }, false);
 }
-
 
 function calculateTotal () {
   var regex = /\[([0-9]+)\]/;
   var elements = document.querySelectorAll(".ItemRow--highlighted, .ItemRow--focused");
-  // Grab highlighted and focused tasks, we don't need to add it length is 1
-  // chrome.alarms.create('UpdatedSelectedInfo', { delayInMinutes: 0.1 });
   if (elements.length > 1) {
     var selectedTask = document.getElementsByClassName("MultiTaskTitleRow-titleText");
-    // See if we've already written the total result before looping
-    console.log('SELECTED TASK', selectedTask);
     for (var j = 0; j < selectedTask.length; j++) {
       var elementsSelected = 0;
       var selected = selectedTask[j];
       var matches = selected.innerHTML.match(regex);
 
       if (!matches) {
-        console.log('No Matches', selectedTask[0].innerHTML, selectedTask.length, selected.innerHTML);
         for (var i = 0; i < elements.length; i++) {
           var element = elements[i];
           var taskName = element.getElementsByClassName("TaskName-shadow"); // Class directly surrounding task name
@@ -59,8 +51,6 @@ function calculateTotal () {
           }
         }
         selected.innerHTML = selected.innerHTML + " [" + elementsSelected.toString() + "] ";
-      } else {
-        console.log('Matches', selectedTask[0].innerHTML, selectedTask.length, selected.innerHTML);
       }
     }
   }
@@ -73,23 +63,24 @@ function checkIfKanBanBoard() {
   }
 }
 
+
 function addColumnTotals() {
   var regex = /\[([0-9]+)\]/;
   var boardColumns = document.getElementsByClassName('BoardColumn');
   for (var i = 0; i < boardColumns.length; i++) {
     var columnTitle = boardColumns[i].getElementsByClassName('BoardColumnHeaderTitle');
-    if(columnTitle[0].innerHTML.includes('[')){}
-    else{
-      var columnPointTotal = 0;
-      // TODO Count Points in card before spitting out the total
-      var columnCardTitles = boardColumns[i].getElementsByClassName('BoardCardWithCustomProperties-name');
-      for(var j = 0; j < columnCardTitles.length; j++) {
-        var matches = columnCardTitles[j].innerHTML.match(regex);
-        if (null != matches) {
-          columnPointTotal = Number(columnPointTotal) + Number(matches[1]);
-        }
-      }
-      columnTitle[0].innerHTML = columnTitle[0].innerHTML + ' [' + columnPointTotal.toString() + ']';
+    if(columnTitle[0].innerHTML.includes('[')) {
+      columnTitle[0].innerHTML = columnTitle[0].innerHTML.replace(regex, '');
     }
+    var columnPointTotal = 0;
+    // TODO Count Points in card before spitting out the total
+    var columnCardTitles = boardColumns[i].getElementsByClassName('BoardCardWithCustomProperties-name');
+    for(var j = 0; j < columnCardTitles.length; j++) {
+      var matches = columnCardTitles[j].innerHTML.match(regex);
+      if (null != matches) {
+        columnPointTotal = Number(columnPointTotal) + Number(matches[1]);
+      }
+    }
+    columnTitle[0].innerHTML = columnTitle[0].innerHTML + ' [' + columnPointTotal.toString() + ']';
   }
 }
